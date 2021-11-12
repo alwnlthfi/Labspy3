b0bae835ebc985266947df2e5418e9850592e914
alwnlthfi <alwanluthfi100@gmail.com>
2021-11-13 00:57:22 +0700

    menambahkan file ss

---
+++ codingan/latihan 1.py
@@ -0,0 +1,14 @@
+print(" Program Menampilkan n bilangan acak yang lebih kecil dari 0.5")
+print(" ---------------------------------------------------------------")
+print()
+
+from random import random
+n=int(input("Masukan Nilai N : "))
+for i in range(n):
+    while 1:
+        n=random()
+        if n < 0.5:
+            break
+    print("data ke: ",i,"=>",n)
+
+print("Selesai")
